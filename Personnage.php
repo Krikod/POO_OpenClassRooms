9a4c3764ba4ae63bb53e17f30f7f2e08f19bb7e3
Krikod <kristen.kod@gmail.com>
2017-11-09 14:54:09 +0100

    script with getters, setters, & other functions

--- Personnage.php
+++ Personnage.php
@@ -4,29 +4,89 @@
 class Personnage
 {
 	// On déclare les attributs
-
 	private $_force; 
-	private $_localisation;
 	private $_experience;
 	private $_degats;
+	private $_localisation;
 
-	// On déclare les méthodes
+// On déclare les méthodes
+// Dans la méthode, on accède à l'attribut _* à l'aide de la pseudo-variable $this pour le modifier.
+	
+// On exige un objet Personnage en param
+	public function frapper(Personnage $persoAFrapper)
+	{
+//$this est l'objet utilisé, le même que $perso
 
-	public function parler() {
+// Ajoute la valeur de la force du personnage qui frappe à l'attribut$_degatsdu personnage frappé. 
+		$persoAFrapper->_degats += $this->_force;
+	}
+
+//Comme l'attribut _experience est privé, on ne peut changer sa valeur à l'extérieur de la classe qu'avec une méthode le permettant
+	public function gagnerExperience()
+	{
+		// $this->_experience = $this->_experience + 1;
+		// $this->_experience += 1;
+		$this->_experience++;
+	}
+
+
+// LISTE DES MUTATEURS (SETTERS), chargés de modifier les attributs $_*
+	public function setForce($force)
+	{
+		if (!is_int($force))
+		{
+			trigger_error('La force d\'un personnage doit être un nombre entier', E_USER_WARNING);
+			return;
+		}
+
+		if ($force > 100) 
+		{
+			trigger_error('La force d\'un personnage ne peut dépasser 100', E_USER_WARNING);
+			return;
+		}
+		$this->_force = $force;
+	}
+
+	public function setExperience($experience)
+	{
+		if (!is_int($experience))
+		{
+			trigger_error('L\'expérience d\'un personnage doit être un nombre entier', E_USER_WARNING);
+			return;
+		}
+
+		if ($experience > 100) 
+		{
+			trigger_error('L\'expérience d\'un personnage ne peut dépasser 100', E_USER_WARNING);
+			return;
+		}
+		$this->_experience = $experience;
+	}
+
+//LISTE DES ASSESSEURS (GETTERS).
+	public function degats()
+	{
+		return $this->_degats;
+	}
+
+	public function force()
+	{
+		return $this->_force;
+	}
+
+	public function experience()
+	{
+		return $this->_experience;
+	}
+
+
+// AUTRES FONCTIONS
+	public function parler() 
+	{
 		echo 'Je suis un personnage';
 	}
 
 	public function deplacer() 
-	{
-
-	}
-
-	public function frapper()
-	{
-
-	}
-
-	public function gagnerExperience()
 	{
 
 	}
--- index.php
+++ index.php
@@ -3,8 +3,26 @@
 include_once 'Personnage.php';
 
 // On instancie (on crée une instance de) la classe Personnage
-
-$perso = new Personnage;
 //on appelle la méthode de l'objet avec l'opérateur ->, qui sert généralement à atteindre un attribut.
 // ==> « va chercher l'objet $perso, et invoque la méthode parler() sur cet objet ». 
-$perso->parler();
+
+$perso1 = new Personnage;
+$perso2 = new Personnage;
+
+$perso1->setForce(10);
+$perso1->setExperience(2);
+
+$perso2->setForce(90);
+$perso2->setExperience(58);
+
+$perso1->frapper($perso2);
+$perso1->gagnerExperience();
+
+$perso2->frapper($perso1);
+$perso2->gagnerExperience();
+
+
+echo 'Le personnage 1 a ', $perso1->force(), ' de force, tandis que le personage 2 a ', $perso2->force(), ' de force.<br>';
+echo 'Le personnage 1 a ', $perso1->experience(), ' d\'experience, tandis que le personnage 2 a ', $perso2->experience(), ' d\'experience.<br>';
+echo 'Le personnage 1 a ', $perso1->degats(), ' de degats, tandis que le personnage 2 a ', $perso2->degats(), ' de dégats.<br>';
+
